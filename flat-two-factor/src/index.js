50db16ad1094da6aab77b0a3a8838d12c353de88
Rowan Heaton <rowan.heaton@stu.mmu.ac.uk>
2018-02-19 14:07:30 +0000

    2fa/enquiries shepherd.js

--- flat-two-factor/src/index.js
+++ flat-two-factor/src/index.js
@@ -9,12 +9,44 @@ let tour = new Shepherd.Tour({
   });
   
   //example step 1
-  tour.addStep('step??', {
-    title: 'Step ??',
-    text: 'This is some example text. blah blah blah',
-    attachTo: '.step-?? bottom',
+  tour.addStep('step1', {
+    title: 'Step 1',
+    text: 'This is the Enquiries Tab this gathers all enquiries from customers •A copy of emails will be sent here  •A copy of calls will come here to listen to  •Part exchange leads will be sent here',
+    attachTo: '.step-1 bottom',
     buttons: [{
-        text: 'Proceed to Step ??',
+        text: 'Proceed to Step 2',
+        action: tour.next,
+        classes: 'o-link o-link--primary'
+    }]
+    //advanceOn: '.docs-link click'
+  });
+  
+    tour.addStep('step2', {
+    title: 'Step 2',
+    text: 'This shows which vehicle the enquiry is about as well as a brief description',
+    attachTo: '.step-2 bottom',
+    buttons: [{
+        text: 'Back to Step 1',
+        action: tour.back,
+        classes: 'o-link'
+      },{
+        text: 'Proceed to Step 3',
+        action: tour.next,
+        classes: 'o-link o-link--primary'
+    }]
+    //advanceOn: '.docs-link click'
+  });
+  
+    tour.addStep('step3', {
+    title: 'Step 3',
+    text: 'Each enquiry will get its own tab shown here, clicking on each one will go into what the enquiry actually is',
+    attachTo: '.step-3 bottom',
+    buttons: [{
+        text: 'Back to Step 2',
+        action: tour.back,
+        classes: 'o-link'
+      },{
+        text: 'Proceed to Step 4',
         action: tour.next,
         classes: 'o-link o-link--primary'
     }]
@@ -22,10 +54,10 @@ let tour = new Shepherd.Tour({
   });
 
 //example last step
-  tour.addStep('step??', {
-    title: 'Step ??',
-    text: 'This is some example text. blah blah blah',
-    attachTo: '.step-?? bottom',
+  tour.addStep('step4', {
+    title: 'Step 4',
+    text: 'To reply to a cusotmer enquiry use thi sbutton to launch your default email client. Only the original enquiry will be logged in the enquiries tab. Or see their contact details below',
+    attachTo: '.step-4 bottom',
     buttons: [{
         text: 'Back to Step ??',
         action: tour.back,
@@ -33,7 +65,6 @@ let tour = new Shepherd.Tour({
       },{
         text: 'Got it!',
         action: function() {
-            document.querySelector(".shepherd-launcher").classList.remove("u-at-hidden");
             return tour.hide();
         },
         classes: 'o-link o-link--primary'
@@ -42,10 +73,3 @@ let tour = new Shepherd.Tour({
   });
      
   tour.start();
-
-document.documentElement.addEventListener("click", function(event) {
-    if (event.target.classList.contains("shepherd-launcher")) {
-        document.querySelector(".shepherd-launcher").classList.add("u-at-hidden");
-        tour.start();
-    }
-});
