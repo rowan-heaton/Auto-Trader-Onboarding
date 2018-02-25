09d9d6dd15f2ba5b20c6f5bdd360bb46ae5ee547
Shuhan Khondoker <shuhankhondoker@gmail.com>
2018-02-25 09:54:30 +0000

    Adding second dealer review file

---
+++ flat-dealer-reviews 2/src/index.js
@@ -0,0 +1,50 @@
+import Shepherd from 'tether-shepherd';
+import './styles/main.scss';
+
+let tour = new Shepherd.Tour({
+    defaults: {
+      classes: 'shepherd-theme-default',
+      scrollTo: true
+    }
+  });
+  
+  //example step 1
+  tour.addStep('step??', {
+    title: 'Step ??',
+    text: 'This is some example text. blah blah blah',
+    attachTo: '.step-?? bottom',
+    buttons: [{
+        text: 'Proceed to Step ??',
+        action: tour.next,
+        classes: 'o-link o-link--primary'
+    }]
+    //advanceOn: '.docs-link click'
+  });
+
+//example last step
+  tour.addStep('step??', {
+    title: 'Step ??',
+    text: 'This is some example text. blah blah blah',
+    attachTo: '.step-?? bottom',
+    buttons: [{
+        text: 'Back to Step ??',
+        action: tour.back,
+        classes: 'o-link'
+      },{
+        text: 'Responce Dashboard',
+        action: function() {
+            window.location.href("../../flat-group-portal/dist/index.htm")
+        },
+        classes: 'o-link o-link--primary'
+    }]
+    //advanceOn: '.docs-link click'
+  });
+     
+  tour.start();
+
+document.documentElement.addEventListener("click", function(event) {
+    if (event.target.classList.contains("shepherd-launcher")) {
+        document.querySelector(".shepherd-launcher").classList.add("u-at-hidden");
+        tour.start();
+    }
+});
