69c30de4925a86712d58a3274553bbd51704d222
Rowan Heaton <rowan.heaton@stu.mmu.ac.uk>
2018-03-05 14:46:17 +0000

    Rowan Shepherd Contents

--- Onboarding-Tour/3.Quick/3.Advertising/2.VRM-Results/src/index.js
+++ Onboarding-Tour/3.Quick/3.Advertising/2.VRM-Results/src/index.js
@@ -31,4 +31,68 @@ let tour = new Shepherd.Tour({
   tour.start();
 
 
+function generateContents() {
+  var totalSteps = document.querySelectorAll(".down-shep");
+  var contentsBuilder = '<div class="contents__wrapper">' + 
+    '<button class="contents__toggle c-at-btn c-at-btn--primary">' +
+      '<span class="contents__toggle-icon"></span>' +
+    '</button>' + 
+    '<div class="contents c-at-card c-at-card--shadow c-at-card--padding">' +
+      '<h2 class="contents__title">Help sections</h2>' +
+      '<ul class="contents__items">';
+
+  for (var i = 0; i < totalSteps.length ; i++) {
+    var stepNo = i + 1;
+    contentsBuilder += '<li class="contents__item">' + 
+      '<button class="contents__section o-button-link" data-step-id="step' + stepNo + '">' + totalSteps[i].getAttribute('data-step-title') + '</button>' + 
+    '</li>';
+  }
+
+  contentsBuilder += '</ul>' + 
+      '</div>' +
+    '</div>';
+
+  return contentsBuilder;
+}
+
+ function insertContents() {
+  document.querySelector(".contents__holder").innerHTML = generateContents();
+}
+
+insertContents();
+
+document.documentElement.addEventListener("click", function(event) {
+  if (event.target.classList.contains("shepherd-launcher")) {
+      document.querySelector(".shepherd-launcher").classList.add("u-at-hidden");
+      tour.start();
+  }
+
+  if (event.target.classList.contains("contents__toggle")) {
+    var contentsSection = document.querySelector(".contents__wrapper");
+
+    if (contentsSection.classList.contains("contents__wrapper--open")) {
+      contentsSection.classList.remove("contents__wrapper--open");
+      document.querySelector(".contents__section--current").classList.remove("contents__section--current");
+    } else {
+      contentsSection.classList.add("contents__wrapper--open");
+      document.querySelector('[data-step-id="' + tour.getCurrentStep().id + '"]').classList.add("contents__section--current");
+    }
+  }
+
+  if (event.target.classList.contains("contents__section")) {
+    var tourId = event.target.getAttribute('data-step-id').replace("step", "");
+    tour.show([tourId-1]);
+
+    document.querySelector(".contents__section--current").classList.remove("contents__section--current");
+    document.querySelector('[data-step-id="' + tour.getCurrentStep().id + '"]').classList.add("contents__section--current");
+  }
+
+  if (event.target.classList.contains("shepherd-button")) {
+    document.querySelector(".contents__section--current").classList.remove("contents__section--current");
+    document.querySelector('[data-step-id="' + tour.getCurrentStep().id + '"]').classList.add("contents__section--current");
+  }
+
+}, false);
+
+
  
